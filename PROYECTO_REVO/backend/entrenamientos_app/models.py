546e90e27261cd7c762d46d671fded59f90b4af5
FernandoGarambelM <fgarambel@unsa.edu.pe>
2024-06-25 21:51:03 -0500

    Creando los modelos para la rutina y el coach

--- PROYECTO_REVO/backend/entrenamientos_app/models.py
+++ PROYECTO_REVO/backend/entrenamientos_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from usuarios_app.models import Usuario
 
 # Create your models here.
+class Rutina(models.Model):
+    nombre_ejercicio = models.CharField(max_length=255)
+    numero_repeticiones = models.IntegerField()
+    dia = models.IntegerField()
+    musculo_ejercitar = models.CharField(max_length=255)
+
+class Coach(models.Model):
+    nombre = models.CharField(max_length=255)
+    rutina = models.ForeignKey(Rutina, on_delete=models.CASCADE)
+    usuarios = models.ManyToManyField(Usuario, related_name='coaches')
