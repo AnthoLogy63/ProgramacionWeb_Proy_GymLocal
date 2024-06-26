89ab8c84f9f11c72d1f611160f648108f0c645e3
FernandoGarambelM <fgarambel@unsa.edu.pe>
2024-06-25 22:55:33 -0500

    Agregando datosfisicos a admin

--- PROYECTO_REVO/backend/entrenamientos_app/admin.py
+++ PROYECTO_REVO/backend/entrenamientos_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Rutina, Coach
+from .models import Rutina, Coach, DatosFisicos
 
 # Register your models here.
 
 admin.site.register(Rutina)
 admin.site.register(Coach)
+admin.site.register(DatosFisicos)
