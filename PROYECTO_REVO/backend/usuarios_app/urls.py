af714234bceb8bd240ba8391e949bce71f05834d
nuevo_63 <ajrahuacso@gmail.com>
2024-06-26 14:35:47 -0500

    Se agregan ciertos estilos para la prueba interna, funciona  el login

--- PROYECTO_REVO/backend/usuarios_app/urls.py
+++ PROYECTO_REVO/backend/usuarios_app/urls.py
@@ -3,6 +3,6 @@ from . views import *
 
 urlpatterns = [
     path('', home, name='home'),
-    path('products/', entrenamientos, name='entrenamientos'),
+    path('entrenamientos/', entrenamientos, name='entrenamientos'),
     path('logout/', exit, name='exit')
 ]
