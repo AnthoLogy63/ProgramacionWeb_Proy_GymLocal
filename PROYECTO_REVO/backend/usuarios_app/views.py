263277028ae32afb3191a655fc7349eb89856006
nuevo_63 <ajrahuacso@gmail.com>
2024-06-26 16:19:15 -0500

    Funciona el register

--- PROYECTO_REVO/backend/usuarios_app/views.py
+++ PROYECTO_REVO/backend/usuarios_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login, authenticate
+from .forms import RegisterForm
 
 def home(request):
     return render(request, 'core/home.html')
@@ -14,4 +15,15 @@ def exit(request):
     return redirect('home') 
 
 def register(request):
-    return render(request, 'registration/register.html')
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('home')  # Redirect to a homepage or any other page
+    else:
+        form = RegisterForm()
+    return render(request, 'registration/register.html', {'form': form})
