6ad8d5a2a8fbdf2f1b35ad9e010f644b836608ba
nuevo_63 <ajrahuacso@gmail.com>
2024-06-29 00:07:48 -0500

    Se agregan requerimientos de nombres y apellidos, y se cambia parcialmente restricciones para el registro

--- PROYECTO_REVO/backend/usuarios_app/models.py
+++ PROYECTO_REVO/backend/usuarios_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Users(AbstractUser):
+    membership_active = models.BooleanField(default=True)
+    avatar = models.ImageField(upload_to='media/', null=True, blank=True)
