9fe503d931adcfc1f5e6ae28790e2cf82eb8877d
nuevo_63 <ajrahuacso@gmail.com>
2024-06-30 15:22:32 -0500

    Lse crea una carpeta para los servicios

--- PROYECTO_REVO/frontend/src/app/core/services/auth.service.ts
+++ PROYECTO_REVO/frontend/src/app/core/services/auth.service.ts
@@ -5,7 +5,7 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class ApiService {
+export class AuthService {
   private apiUrl = 'http://localhost:8000/api';
   private currentUser: string | null = null;
   
