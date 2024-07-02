ac5d57dde0c1f744787ac3ee0fbc835818291eb5
wchoquehuancab <wchoquehuancab@unsa.edu.pe>
2024-07-01 23:26:47 -0500

    Se define las direcciones de los locales

--- PROYECTO_REVO/frontend/src/app/components/espacios/espacios.component.ts
+++ PROYECTO_REVO/frontend/src/app/components/espacios/espacios.component.ts
@@ -9,5 +9,26 @@ import { ApiAuthService } from '../../core/services/api-auth.service';
   styleUrl: './espacios.component.css'
 })
 export class EspaciosComponent {
-
+  locales = [
+    {
+      sede: 'SEDE CENTRO',
+      direccion: 'CALLE PAUCARPATA 302 CERCADO',
+      referencia: '(FRENTE A LA UNSA)'
+    },
+    {
+      sede: 'SEDE NORTE',
+      direccion: 'AV. NORTE 123',
+      referencia: '(CERCA AL PARQUE NORTE)'
+    },
+    {
+      sede: 'SEDE SUR',
+      direccion: 'AV. SUR 456',
+      referencia: '(AL LADO DEL CENTRO COMERCIAL SUR)'
+    },
+    {
+      sede: 'SEDE ESTE',
+      direccion: 'CALLE ESTE 789',
+      referencia: '(AL FRENTE DEL ESTADIO ESTE)'
+    }
+  ];
 }
