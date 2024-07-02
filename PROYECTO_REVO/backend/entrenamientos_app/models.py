e85c8c136056818cfc01dbc8221446e97bc351ce
FernandoGarambelM <fgarambel@unsa.edu.pe>
2024-07-02 10:34:05 -0500

    Import user

--- PROYECTO_REVO/backend/entrenamientos_app/models.py
+++ PROYECTO_REVO/backend/entrenamientos_app/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from usuarios_app.models import Usuario
+from usuarios_app.models import User
 
 # Create your models here.
 class DatosFisicos(models.Model):
-    usuario = models.OneToOneField(Usuario, on_delete=models.CASCADE)
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
     edad = models.IntegerField()
     peso = models.FloatField()
     talla = models.FloatField()
@@ -20,4 +20,4 @@ class Rutina(models.Model):
 class Coach(models.Model):
     nombre = models.CharField(max_length=255)
     rutina = models.OneToOneField(Rutina, on_delete=models.CASCADE)
-    usuarios = models.ManyToManyField(Usuario, related_name='coaches')
+    usuarios = models.ManyToManyField(User, related_name='coaches')
