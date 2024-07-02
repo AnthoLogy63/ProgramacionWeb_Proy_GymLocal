49c34e9e09daa38138a434ebb90a190758aff6cc
wchoquehuancab <wchoquehuancab@unsa.edu.pe>
2024-07-01 23:07:24 -0500

    agregando la ruta para espacios

--- PROYECTO_REVO/frontend/src/app/app.routes.ts
+++ PROYECTO_REVO/frontend/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
+import { EspaciosComponent } from './components/espacios/espacios.component';
 
 export const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'espacios', component: EspaciosComponent },
 ];
    
