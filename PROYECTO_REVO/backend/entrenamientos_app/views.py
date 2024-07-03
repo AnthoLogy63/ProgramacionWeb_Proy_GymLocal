058718a1675d7f1e871a0b104df4d11e35b40e4a
FernandoGarambelM <fgarambel@unsa.edu.pe>
2024-07-03 00:06:33 -0500

    Configurando API para el envio de datos

--- PROYECTO_REVO/backend/entrenamientos_app/views.py
+++ PROYECTO_REVO/backend/entrenamientos_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Entrenamiento
+from .serializers import EntrenamientoSerializer
 
 # Create your views here.
+
+class EntrenamientoViewSet(viewsets.ModelViewSet):
+    queryset = Entrenamiento.objects.all()
+    serializer_class = EntrenamientoSerializer
