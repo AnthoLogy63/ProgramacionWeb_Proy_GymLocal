60d419e5f9c0e2d8632805713f03bcb3ebfaa2e2
nuevo_63 <ajrahuacso@gmail.com>
2024-07-03 14:21:49 -0500

    Cambios del backend y login y register concluidos

--- PROYECTO_REVO/backend/usuarios_app/models.py
+++ PROYECTO_REVO/backend/usuarios_app/models.py
@@ -3,4 +3,4 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     membership_active = models.BooleanField(default=True)
-    avatar = models.ImageField(default='userDefect.jpg', upload_to='users/', null=True, blank=True)
+    avatar = models.ImageField(default='userDefect.png', upload_to='users/', null=True, blank=True)
