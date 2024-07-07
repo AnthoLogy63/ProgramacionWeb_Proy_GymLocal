8d1fd29a7f50dcfc240cbb52eef99df9ef1d3f05
nuevo_63 <ajrahuacso@gmail.com>
2024-07-07 15:12:46 -0500

    Configuración de ruta

--- PROYECTO_REVO/backend/backend/settings.py
+++ PROYECTO_REVO/backend/backend/settings.py
@@ -147,7 +147,7 @@ AUTH_USER_MODEL = 'usuarios_app.User'
 
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
