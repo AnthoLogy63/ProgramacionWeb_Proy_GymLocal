8e9d34ec1653949cb0730e2743a9e825b03918ff
FernandoGarambelM <fgarambel@unsa.edu.pe>
2024-07-07 14:28:11 -0500

    Configuramos los datos para el envio de email

--- PROYECTO_REVO/backend/backend/settings.py
+++ PROYECTO_REVO/backend/backend/settings.py
@@ -147,3 +147,10 @@ AUTH_USER_MODEL = 'usuarios_app.User'
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'gymrevo94@gmail.com'
+EMAIL_HOST_PASSWORD = 'cmbh cufb xdqi bdth'
