175ab08bf18ea78dec4a1a909dc8f80ab6973d54
FernandoGarambelM <fgarambel@unsa.edu.pe>
2024-07-06 22:49:25 -0500

    Urls para datos

--- PROYECTO_REVO/backend/entrenamientos_app/urls.py
+++ PROYECTO_REVO/backend/entrenamientos_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import DatosFisicosViewSet, RutinaViewSet, CoachViewSet
+from .views import get_datos_fisicos, get_rutinas, get_coaches , get_user_id
 
 router = DefaultRouter()
 router.register(r'datos_fisicos', DatosFisicosViewSet)
@@ -8,5 +9,9 @@ router.register(r'rutinas', RutinaViewSet)
 router.register(r'coaches', CoachViewSet)
 
 urlpatterns = [
+    path('user_id/', get_user_id, name='get_user_id'),
+    path('datos_fisicos/<int:user_id>/', get_datos_fisicos, name='get_datos_fisicos'),
+    path('rutinas/<int:user_id>/', get_rutinas, name='get_rutinas'),
+    path('coaches/<int:user_id>/', get_coaches, name='get_coaches'),
     path('', include(router.urls)),
 ]
