cda864ba383efd04ec53bd47beb3c6cab2363e42
nuevo_63 <ajrahuacso@gmail.com>
2024-07-07 18:38:07 -0500

    Se genera vista en el register para redireccionar al login

--- PROYECTO_REVO/frontend/src/app/components/register/register.component.ts
+++ PROYECTO_REVO/frontend/src/app/components/register/register.component.ts
@@ -2,13 +2,13 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { ApiRegUserService } from '../../core/services/api-reg-user.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
