4b512a1da994412c6e56901efff6467b5e669f21
FernandoGarambelM <fgarambel@unsa.edu.pe>
2024-07-07 14:28:46 -0500

    Se agrega url al API

--- PROYECTO_REVO/backend/entrenamientos_app/urls.py
+++ PROYECTO_REVO/backend/entrenamientos_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import DatosFisicosViewSet, RutinaViewSet, CoachViewSet
 from .views import get_datos_fisicos, get_rutinas, get_coaches , get_user_id
-from .views import download_training_pdf
+from .views import download_training_pdf, send_training_pdf
 
 router = DefaultRouter()
 router.register(r'datos_fisicos', DatosFisicosViewSet)
@@ -15,5 +15,6 @@ urlpatterns = [
     path('rutinas/<int:user_id>/', get_rutinas, name='get_rutinas'),
     path('coaches/<int:user_id>/', get_coaches, name='get_coaches'),
     path('download_training_pdf/', download_training_pdf, name='download_training_pdf'),
+    path('send_training_pdf/', send_training_pdf, name='send_training_pdf'),
     path('', include(router.urls)),
 ]
