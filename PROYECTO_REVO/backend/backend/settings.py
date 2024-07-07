e6d58351a8975ea383d86be8beec4c8e502ac517
nuevo_63 <ajrahuacso@gmail.com>
2024-07-07 14:27:31 -0500

    Se modifican aspectos para mostrar los perfiles

--- PROYECTO_REVO/backend/backend/settings.py
+++ PROYECTO_REVO/backend/backend/settings.py
@@ -145,5 +145,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'usuarios_app.User'
 
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
--- PROYECTO_REVO/backend/usuarios_app/models.py
+++ PROYECTO_REVO/backend/usuarios_app/models.py
@@ -3,4 +3,9 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     membership_active = models.BooleanField(default=True)
-    avatar = models.ImageField(default='userDefect.png', upload_to='users/', null=True, blank=True)
+    avatar = models.ImageField(default='userDefect.png', upload_to='users/', blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.avatar:
+            self.avatar = 'userDefect.png'
+        super().save(*args, **kwargs)
