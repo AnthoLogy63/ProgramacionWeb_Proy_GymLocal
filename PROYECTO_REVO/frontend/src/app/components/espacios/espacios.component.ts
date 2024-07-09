af62f96940bed3fffc3101fb1fbd84d31afd814d
wchoquehuancab <wchoquehuancab@unsa.edu.pe>
2024-07-09 13:10:23 -0500

    fix direcitons of spacios page

--- PROYECTO_REVO/frontend/src/app/components/espacios/espacios.component.ts
+++ PROYECTO_REVO/frontend/src/app/components/espacios/espacios.component.ts
@@ -29,13 +29,13 @@ export class EspaciosComponent {
       sede: 'SEDE BUSTAMANTE',
       direccion: 'AV. DOLORES 163',
       referencia: '(CERCA DEL COLEGIO PAMER AREQUIPA)',
-      mapaUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827.258258197388!2d-71.559520423967!3d-16.411705038460184!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91424b2bb2571d19%3A0x5558a9a5f2cb900b!2sRevo%20Sport%20Sachaca!5e0!3m2!1ses!2spe!4v1720240904301!5m2!1ses!2spe'
+      mapaUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3826.930022521709!2d-71.52848081432452!3d-16.42837999840003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91424b20604bf951%3A0x831dfdcd15669b30!2sRevo%20Sport%20-%20Sede%20Bustamante!5e0!3m2!1ses!2spe!4v1720548434164!5m2!1ses!2spe'
     },
     {
       sede: 'SEDE SACHACA',
       direccion: 'VARIANTE DE UCHUCAMAYO 5',
       referencia: '(AL FRENTE DE LA PLANTA BACKUS)',
-      mapaUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827.9036465736062!2d-71.56044539269598!3d-16.378870010491756!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91424a11c8e84a71%3A0xe2cff0b88bb16b97!2sRevo%20Sport%20Cerro%20Colorado!5e0!3m2!1ses!2spe!4v1720240972006!5m2!1ses!2spe'
+      mapaUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3827.258258197388!2d-71.559520423967!3d-16.411705038460184!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91424b2bb2571d19%3A0x5558a9a5f2cb900b!2sRevo%20Sport%20Sachaca!5e0!3m2!1ses!2spe!4v1720240904301!5m2!1ses!2spe'
     }
   ];
 
